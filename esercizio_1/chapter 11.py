998bc3d5bc428d4b28f64d5f0d7e5f80120c8196
navyzigz420 <leonardo.passani@gmail.com>
2018-09-05 16:49:20 -0400

    chapter11

---
+++ esercizio_1/chapter 11.py
@@ -0,0 +1,22 @@
+import turtle
+t = turtle.Pen()
+t.color(1,0,0)
+
+def octagon(size, red, green, blue, outline):
+    t.color(red, green, blue)
+    t.begin_fill()
+    for x in range(0, 8):
+        t.forward(size)
+        t.right(45)
+    t.end_fill()
+    if outline == True:
+        t.color(0,0,0)
+        for x in range(0, 8):
+            t.forward(size)
+            t.right(45)
+octagon(100, 1,0,1, True)
+
+
+    
+        
+    
